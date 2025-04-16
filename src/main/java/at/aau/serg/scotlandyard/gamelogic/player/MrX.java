e78c874c1b1205b69bfefebb5e8a5f21aaa44cee
noxar007 <manuel.stingl@gmx.at>
2025-04-16 14:45:53 +0200

    added MrX initalizeTickets

--- src/main/java/at/aau/serg/scotlandyard/gamelogic/player/MrX.java
+++ src/main/java/at/aau/serg/scotlandyard/gamelogic/player/MrX.java
@@ -1,15 +1,29 @@
 package at.aau.serg.scotlandyard.gamelogic.player;
 
-import at.aau.serg.scotlandyard.gamelogic.board.Transport;
+import at.aau.serg.scotlandyard.gamelogic.player.tickets.PlayerTickets;
+import at.aau.serg.scotlandyard.gamelogic.player.tickets.Ticket;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MrX extends Player {
-    int doubleTickets;
+    private int doubleTicket;
 
     public MrX() {
-        super();
-        ticketsLeft.put(Transport.taxi,0);
-        ticketsLeft.put(Transport.bus,0);
-        ticketsLeft.put(Transport.underground,0);
-        ticketsLeft.put(Transport.BOAT,5);
+        super(initializeTickets());
+        doubleTicket = 2;
     }
+    private static PlayerTickets initializeTickets() {
+        Map<Ticket, Integer> initialTickets = new HashMap<>();
+
+        initialTickets.put(Ticket.TAXI, 4);
+        initialTickets.put(Ticket.BUS, 3);
+        initialTickets.put(Ticket.TRAIN, 3);
+        initialTickets.put(Ticket.BLACK, 5);
+        initialTickets.put(Ticket.DOUBLE, 2);  // Falls Doppelzüge erlaubt sind
+
+        return new PlayerTickets(initialTickets);
+    }
+
+
 }
