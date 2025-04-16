83322ac1507464408d2e4a6f9903edb98f1be019
noxar007 <manuel.stingl@gmx.at>
2025-04-16 14:15:32 +0200

    added use, add and get ticket methods

--- src/main/java/at/aau/serg/scotlandyard/gamelogic/player/tickets/PlayerTickets.java
+++ src/main/java/at/aau/serg/scotlandyard/gamelogic/player/tickets/PlayerTickets.java
@@ -13,6 +13,21 @@ public class PlayerTickets {
     public boolean hasTicket(Ticket ticket) {
         return tickets.getOrDefault(ticket, 0) > 0;
     }
+
+    public void useTicket(Ticket ticket) {
+        if (!hasTicket(ticket)) {
+            throw new IllegalStateException("Spieler hat dieses Ticket nich");
+        }
+        tickets.put(ticket, tickets.get(ticket) - 1);
+    }
+
+    public void addTicket(Ticket ticket) {
+        tickets.put(ticket, tickets.getOrDefault(ticket, 0) + 1);
+    }
+
+    public int getTicketCount(Ticket ticket) {
+        return tickets.getOrDefault(ticket, 0);
+    }
 }
 
 
