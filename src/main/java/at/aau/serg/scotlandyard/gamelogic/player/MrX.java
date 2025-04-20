b51c665422a6043416810020b190ffa4cb740fd5
noxar007 <manuel.stingl@gmx.at>
2025-04-20 10:28:56 +0200

    deleted double Ticket in MrXClass
    Ticket is in the Ticketsystem.

--- src/main/java/at/aau/serg/scotlandyard/gamelogic/player/MrX.java
+++ src/main/java/at/aau/serg/scotlandyard/gamelogic/player/MrX.java
@@ -7,11 +7,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MrX extends Player {
-    private int doubleTicket;
-
+    
     public MrX() {
         super(initializeTickets());
-        doubleTicket = 2;
+
     }
     private static PlayerTickets initializeTickets() {
         Map<Ticket, Integer> initialTickets = new HashMap<>();
@@ -25,14 +24,5 @@ public class MrX extends Player {
         return new PlayerTickets(initialTickets);
     }
 
-    public boolean canUseDoubleTicket() {
-        return doubleTicket > 0;
-    }
 
-    public void useDoubleTicket() {
-        if (!canUseDoubleTicket()) {
-            throw new IllegalStateException("No DOUBLE tickets left!");
-        }
-        doubleTicket--;
-    }
 }
