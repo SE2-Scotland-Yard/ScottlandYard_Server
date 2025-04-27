c028d966194c6188faa4e420006d861773b0a617
Johanna Hasler <johasler@edu.aau.at>
2025-04-27 12:10:11 +0200

    Add moveDouble

--- src/main/java/at/aau/serg/scotlandyard/gamelogic/player/MrX.java
+++ src/main/java/at/aau/serg/scotlandyard/gamelogic/player/MrX.java
@@ -1,5 +1,6 @@
 package at.aau.serg.scotlandyard.gamelogic.player;
 
+import at.aau.serg.scotlandyard.gamelogic.board.Board;
 import at.aau.serg.scotlandyard.gamelogic.player.tickets.PlayerTickets;
 import at.aau.serg.scotlandyard.gamelogic.player.tickets.Ticket;
 
@@ -7,11 +8,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MrX extends Player {
-    
+
     public MrX() {
         super(initializeTickets());
-
     }
+
     private static PlayerTickets initializeTickets() {
         Map<Ticket, Integer> initialTickets = new HashMap<>();
 
@@ -24,5 +25,35 @@ public class MrX extends Player {
         return new PlayerTickets(initialTickets);
     }
 
+    public void moveDouble(int firstTo, Ticket firstTicket, int secondTo, Ticket secondTicket, Board board) {
+        if (!tickets.hasTicket(Ticket.DOUBLE)) {
+            throw new IllegalArgumentException("Kein DOUBLE-Ticket verfügbar!");
+        }
 
+        // Prüfe beide Züge
+        if (!isValidMove(firstTo, firstTicket, board)) {
+            throw new IllegalArgumentException("Erster Zug ungültig!");
+        }
+
+        // Simuliere erste Bewegung temporär, um zweiten Zug zu validieren
+        int originalPos = this.pos;
+        this.pos = firstTo;
+
+        boolean validSecond = isValidMove(secondTo, secondTicket, board);
+        this.pos = originalPos; // Position zurücksetzen
+
+        if (!validSecond) {
+            throw new IllegalArgumentException("Zweiter Zug ungültig!");
+        }
+
+        // Zug ist gültig → Tickets verwenden und Position setzen
+        tickets.useTicket(Ticket.DOUBLE);
+        tickets.useTicket(firstTicket);
+        this.pos = firstTo;
+
+        tickets.useTicket(secondTicket);
+        this.pos = secondTo;
+
+        System.out.println("MrX machte einen Doppelzug: " + firstTo + " → " + secondTo);
+    }
 }
