2fc533d5939babe536d6d6b3e15b417151bbc79f
eliasra <eliasra@edu.aau.at>
2025-05-12 00:29:42 +0200

    added test for markPlayer()

--- src/test/java/at/aau/serg/scotlandyard/gamelogic/LobbyTest.java
+++ src/test/java/at/aau/serg/scotlandyard/gamelogic/LobbyTest.java
@@ -32,10 +32,42 @@ class LobbyTest {
     }
 
     @Test
+    void testAddPlayerDuplicate() {
+        lobby.addPlayer("Player");
+        boolean success = lobby.addPlayer("Player");
+        assertFalse(success);
+        assertEquals(1, lobby.getPlayers().size());
+    }
+
+    @Test
+    void testAddPlayerStarted() {
+        lobby.addPlayer("Player1");
+        lobby.markStarted();
+        boolean success = lobby.addPlayer("Player2");
+        assertFalse(success);
+        assertEquals(1, lobby.getPlayers().size());
+    }
+
+    @Test
     void testRemovePlayer() {
         lobby.addPlayer("Player");
         boolean success = lobby.removePlayer("Player");
         assertTrue(success);
         assertEquals(0, lobby.getPlayers().size());
     }
+
+    @Test
+    void testMarkReady(){
+        lobby.addPlayer("Player");
+        boolean success = lobby.markReady("Player");
+        assertTrue(success);
+        assertEquals(1, lobby.getReadyStatus().size());
+    }
+
+    @Test
+    void testMarkReadyNoPlayer(){
+        boolean success = lobby.markReady("Nothing");
+        assertFalse(success);
+    }
+
 }
