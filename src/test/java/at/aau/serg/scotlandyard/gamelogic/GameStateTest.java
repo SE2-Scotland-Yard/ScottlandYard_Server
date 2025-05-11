a8a096551452728c9b8ba229d61801a92c034154
eliasra <eliasra@edu.aau.at>
2025-05-11 20:40:12 +0200

    added tests for getAllowedMoves

--- src/test/java/at/aau/serg/scotlandyard/gamelogic/GameStateTest.java
+++ src/test/java/at/aau/serg/scotlandyard/gamelogic/GameStateTest.java
@@ -4,6 +4,7 @@ import at.aau.serg.scotlandyard.gamelogic.board.Edge;
 import at.aau.serg.scotlandyard.gamelogic.player.Detective;
 import at.aau.serg.scotlandyard.gamelogic.player.MrX;
 import at.aau.serg.scotlandyard.gamelogic.player.Player;
+import at.aau.serg.scotlandyard.gamelogic.player.tickets.PlayerTickets;
 import at.aau.serg.scotlandyard.gamelogic.player.tickets.Ticket;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,7 @@ import java.util.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 public class GameStateTest {
@@ -37,6 +39,33 @@ public class GameStateTest {
         assertEquals(2, gameState.getAllPlayers().size());
         assertTrue(gameState.getAllPlayers().containsKey("MrX"));
         assertTrue(gameState.getAllPlayers().containsKey("Detective"));
+    }
+
+    @Test
+    void testGetAllowedMoves(){
+        when(mrX.getPosition()).thenReturn(1);
+        when(mrX.getTickets()).thenReturn(getDefaultTickets());
+
+        List<Integer> allowedMoves = gameState.getAllowedMoves("MrX");
+        assertEquals(5, allowedMoves.size());
+    }
+
+    private static PlayerTickets getDefaultTickets() {
+        Map<Ticket, Integer> initialTickets = new EnumMap<>(Ticket.class);
+
+        initialTickets.put(Ticket.TAXI, 4);
+        initialTickets.put(Ticket.BUS, 3);
+        initialTickets.put(Ticket.UNDERGROUND, 3);
+        initialTickets.put(Ticket.BLACK, 5);
+        initialTickets.put(Ticket.DOUBLE, 2);
+
+        return new PlayerTickets(initialTickets);
+    }
+
+    @Test
+    void testGetAllowedMovesPlayerIsNull(){
+        List<Integer> allowedMoves = gameState.getAllowedMoves("Nothing");
+        assertTrue(allowedMoves.isEmpty());
     }
 
 
